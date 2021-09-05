d2d85ffdf011758c91b08cc7f650de5084fc22a5
Emanuel <emanuelX92@gmail.com>
2021-09-04 20:22:08 -0400

    start

---
+++ rutismo/rutismo/settings/base.py
@@ -0,0 +1,123 @@
+"""
+Django settings for rutismo project.
+
+using Django 3.2.6.
+
+"""
+
+from pathlib import Path
+from django.apps import AppConfig
+import os
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+AppConfig.default = False
+# Application definition
+
+BASE_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+
+]
+
+LOCAL_APPS = [
+
+    'apps.authentication',
+
+]
+THIRD_APPS = [
+    #'corsheaders',
+    'rest_framework',
+    #'django_rest_passwordreset',
+    # 'django_filters',
+
+
+]
+
+INSTALLED_APPS = BASE_APPS+LOCAL_APPS+THIRD_APPS
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'rutismo.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'rutismo.wsgi.application'
+
+
+# Password validation
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+LANGUAGE_CODE = 'en-ve'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+#Agregamos las urls que puedan hacerle peticiones de tipo Cors a django, en este caso Vuejs
+CORS_ORIGIN_WHITELIST = ["http://localhost:8080"]
+CORS_ALLOW_CREDENTIALS = True
+
+
+#configuration media root for manage to file
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # path to server local
+MEDIA_URL = 'http://localhost:8000/media/'  # URL for developer
+
+
+# Default primary key field type
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ rutismo/rutismo/settings/local.py
@@ -0,0 +1,31 @@
+from .base import *
+import environ
+
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env('DEBUG')
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('POSTGRESQL_NAME'),
+        'USER': env('POSTGRESQL_USER'),
+        'PASSWORD': env('POSTGRESQL_PASS'),
+        'HOST': env('POSTGRESQL_HOST'),
+        'PORT': env('POSTGRESQL_PORT'),
+    }
+}
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = '/static/'
